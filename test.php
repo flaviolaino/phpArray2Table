feb168bc707f32d8f7fd8a8dd5effb93f09146f5
laino <flavio.laino@immobiliare.it>
2017-11-24 12:27:40 +0100

    init from local

---
+++ test.php
@@ -0,0 +1,41 @@
+<?php
+
+require 'phpArray2Table.php';
+
+$data = [
+    [
+        'firstname' => 'Mollie',
+        'surname' => 'Alvarez',
+        'email' => 'molliealvarez@example.com',
+    ],
+    [
+        'firstname' => 'Dianna',
+        'surname' => 'Mcbride',
+        'age' => 43,
+        'email' => 'diannamcbride@example.com',
+    ],
+    [
+        'firstname' => 'Elvira',
+        'surname' => 'Mueller',
+        'age' => 50,
+        'email' => 'elviramueller@example.com',
+    ],
+    [
+        'firstname' => 'Corine',
+        'surname' => 'Morton',
+        'age' => 35,
+    ],
+    [
+        'firstname' => 'James',
+        'surname' => 'Allison',
+    ],
+    [
+        'firstname' => 'Bowen',
+        'surname' => 'Kelley',
+        'age' => 50,
+        'email' => 'bowenkelley@example.com',
+    ],
+];
+
+$renderer = new phpArray2Table($data);
+echo $renderer->getTable();
